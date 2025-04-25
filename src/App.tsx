2904bcc8b9d50842dc76abf63e3619266de16be4
gustavo <kg.2k.pl@gmail.com>
2025-04-24 20:25:57 -0400

    simplify CalorieTracker component with Context

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useReducer } from "react"
+import { useEffect, useMemo } from "react"
 import Form from "./components/Form"
 import ActivityList from "./components/ActivityList"
 import CalorieTracker from "./components/CalorieTracker"
@@ -40,9 +40,7 @@ function App() {
 
       <section className="bg-gray-800 py-10">
         <div className="max-w-4xl mx-auto">
-          <CalorieTracker
-            activities={state.activities}
-          />
+          <CalorieTracker />
         </div>
       </section>
 
--- src/context/ActivityContext.tsx
+++ src/context/ActivityContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, Dispatch, ReactNode, useReducer } from "react";
+import { createContext, Dispatch, ReactNode, useMemo, useReducer } from "react";
 import { ActivityActions, activityReducer, ActivityState, initialState } from "../reducers/activity-reducer";
 
 type ActivityProviderProps = {
@@ -8,18 +8,34 @@ type ActivityProviderProps = {
 type ActivityContextProps = {
     state: ActivityState
     dispatch: Dispatch<ActivityActions>
+    caloriesConsumed: number
+    caloriesBurned: number
+    caloriesDifference: number
 }
 
 export const ActivityContext = createContext<ActivityContextProps>(null!);
 
-export const ActivityProvider = ({ children } : ActivityProviderProps) => {
+export const ActivityProvider = ({ children }: ActivityProviderProps) => {
 
     const [state, dispatch] = useReducer(activityReducer, initialState)
+
+    // Counters
+    const caloriesConsumed = useMemo(() => state.activities.reduce(
+        (total, activity) => activity.category === 1 ? total + activity.calories : total, 0), [state.activities]
+    )
+    const caloriesBurned = useMemo(() => state.activities.reduce(
+        (total, activity) => activity.category === 2 ? total + activity.calories : total, 0), [state.activities]
+    )
+    const caloriesDifference = useMemo(() => caloriesConsumed - caloriesBurned, [state.activities]
+    )
 
     return (
         <ActivityContext.Provider value={{
             state,
-            dispatch
+            dispatch,
+            caloriesConsumed,
+            caloriesBurned,
+            caloriesDifference
         }}>
             {children}
         </ActivityContext.Provider>
